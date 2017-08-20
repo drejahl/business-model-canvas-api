79c67037387f1f818dcac9ceb45a2d67e229acfe
Dirk Rejahl <dirk@digiglu.co.uk>
2017-08-20 10:08:06 +0100

    authentication

--- app.js
+++ app.js
@@ -3,21 +3,33 @@
 var app = require("express")();
 var swaggerTools = require("swagger-tools");
 var YAML = require("yamljs");
-var auth = require("./api/helpers/auth");
 var swaggerConfig = YAML.load("./api/swagger/swagger.yaml");
+const cors = require('cors');
+
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+const authenticate = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://digiglu.eu.auth0.com/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  aud: 'http://localhost:10510/v1/',
+  iss: `https://digiglu.eu.auth0.com/`,
+  algorithms: ['RS256']
+});
 
 swaggerTools.initializeMiddleware(swaggerConfig, function(middleware) {
-  //Serves the Swagger UI on /docs
 
-  console.log("Initializing security middleware...")
-  app.use(middleware.swaggerUi());
-  app.use(
-    middleware.swaggerSecurity({
-      //manage token function in the 'auth' module
-      Bearer: auth.verifyToken
-    })
-  );
-  app.use(middleware.swaggerMetadata()); // needs to go after swaggerSecurity, otherwise using 'Authorization' header from swagger api_key doesn't authenticate properly
+  app.use( cors() );
+  app.use( authenticate );
+
+  app.use(middleware.swaggerMetadata()); 
 
   var routerConfig = {
     controllers: "./api/controllers",
@@ -25,6 +37,7 @@ swaggerTools.initializeMiddleware(swaggerConfig, function(middleware) {
   };
 
   app.use(middleware.swaggerRouter(routerConfig));
+  
   app.listen(10510, function() {
     console.log("Started server on port 10510");
   });
