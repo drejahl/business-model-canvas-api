224dbc1de1fd9e4ecd0b22d1d503170d5a5c4f52
Dirk Rejahl <dirk@digiglu.co.uk>
2017-12-27 17:40:53 +0000

    port

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ swaggerTools.initializeMiddleware(swaggerConfig, function(middleware) {
 
   app.use(middleware.swaggerRouter(routerConfig));
 
-  app.listen(process.env.PORT, function() {
+  app.listen(3000, function() {
     console.log("Started server on port ", process.env.PORT);
   });
 });
